59cf18abc3db6af4400f73a0ece85d2e050faf37
Thomas Donnelly <donnelly.thomas21@gmail.com>
2018-12-27 14:24:12 -0500

    refactor

---
+++ app-swoosh/app-swoosh/View/Controller/SkillVC.swift
@@ -0,0 +1,30 @@
+//
+//  SkillVC.swift
+//  app-swoosh
+//
+//  Created by Thomas Donnelly on 12/27/18.
+//  Copyright © 2018 Thomas Donnelly. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
